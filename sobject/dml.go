a75c7f7d723f759a039ad36aa96c1766e5de4574
photodisorder <photodisorder@gmail.com>
2021-01-29 16:25:47 +0100

    Update dml.go

--- sobject/dml.go
+++ sobject/dml.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/g8rswimmer/go-sfdc"
-	"github.com/g8rswimmer/go-sfdc/session"
+	"github.com/photodisorder/go-sfdc"
+	"github.com/photodisorder/go-sfdc/session"
 )
 
 // InsertValue is the value that is returned when a
