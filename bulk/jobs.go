515869fddd942e2b73873d33e78b11fab45087f3
photodisorder <photodisorder@gmail.com>
2021-01-29 16:23:18 +0100

    Update jobs.go

--- bulk/jobs.go
+++ bulk/jobs.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"strconv"
 
-	sfdc "github.com/g8rswimmer/go-sfdc"
-	"github.com/g8rswimmer/go-sfdc/session"
+	sfdc "github.com/photodisorder/go-sfdc"
+	"github.com/photodisorder/go-sfdc/session"
 )
 
 // Parameters to query all of the bulk jobs.
