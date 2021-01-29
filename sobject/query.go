0b92ad5451f783b4abd93681b656cf98eba6acd8
photodisorder <photodisorder@gmail.com>
2021-01-29 16:26:43 +0100

    Update query.go

--- sobject/query.go
+++ sobject/query.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/g8rswimmer/go-sfdc"
-	"github.com/g8rswimmer/go-sfdc/session"
+	"github.com/photodisorder/go-sfdc"
+	"github.com/photodisorder/go-sfdc/session"
 )
 
 // Querier is the interface used to query a SObject from
